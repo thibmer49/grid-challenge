beb3807cf4dd0180850267d2c85fbb69f581075a
Thibault Mercier <thib.mercier@ymail.com>
2023-05-04 19:22:26 +0200

    reorganization

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const grids = document.querySelectorAll(".parent > div");
+// const grids = document.querySelectorAll(".parent > div");
 
-grids.forEach(grid => {
-  grid.addEventListener("mouseover", (event) => {
-    grid.classList.add("hovered");
-    grid.style.transitionProperty = "opacity"
-    grid.style.transitionDuration = "1s";
-    grid.style.transitionTimingFunction = "ease"
-  });
+// grids.forEach(grid => {
+//   grid.addEventListener("mouseover", (event) => {
+//     grid.classList.add("hovered");
+//     grid.style.transitionProperty = "opacity"
+//     grid.style.transitionDuration = "1s";
+//     grid.style.transitionTimingFunction = "ease"
+//   });
 
-  grid.addEventListener("mouseout", (event) => {
-    grid.classList.remove("hovered");
-    grid.style.transitionProperty = "opacity"
-    grid.style.transitionDuration = "1s";
-    grid.style.transitionTimingFunction = "ease"
-  });
-});
+//   grid.addEventListener("mouseout", (event) => {
+//     grid.classList.remove("hovered");
+//     grid.style.transitionProperty = "opacity"
+//     grid.style.transitionDuration = "1s";
+//     grid.style.transitionTimingFunction = "ease"
+//   });
+// });
